b3fcfeaa6c7740e3a98d315b350cf0725b038f34
brian <bapipi@prateducacio.com>
2023-12-17 13:04:39 +0100

    '#'	modified:   src/App.js '#'	modified:   src/pages/Login.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-// En App.
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Login from './pages/Login';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/Login" element={<Home />} />
       </Routes>
     </Router>
   );
 };
 
 export default App;
-
 
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,58 @@
-// Login.js
+// En Login.js
 import React, { useState } from 'react';
-import LoginForm from './components/LoginForm';
-import Dashboard from './components/Dashboard';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+  const [users, setUsers] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleLogin = (loggedInUser) => {
-    setUser(loggedInUser);
+  const handleLogin = () => {
+    // Lógica de inicio de sesión aquí (puede ser simplemente redirigir por ahora)
+    navigate('/home');
+  };
+
+  const handleRegister = () => {
+    // Lógica de registro de usuario
+    // Aquí puedes agregar validaciones y lógica de almacenamiento
+    const newUser = { username, password };
+    setUsers([...users, newUser]);
   };
 
   return (
     <div>
-      {!user ? (
-        <LoginForm onLogin={handleLogin} />
-      ) : (
-        <Dashboard user={user} />
-      )}
+      <h1>Login</h1>
+      <form>
+        {/* Campos de usuario y contraseña */}
+        <label>
+          Usuario:
+          <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Contraseña:
+          <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <br />
+        <button type="button" onClick={handleLogin}>
+          Iniciar Sesión
+        </button>
+        {/* Agregar el botón de registro */}
+        <button type="button" onClick={handleRegister}>
+          Registrarse
+        </button>
+      </form>
+
+      {/* Mostrar la lista de usuarios registrados */}
+      <div>
+        <h2>Usuarios Registrados</h2>
+        <ul>
+          {users.map((user, index) => (
+            <li key={index}>{user.username}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
